be0b4d3d532edd848e57e782ef6e52c0010d6430
Dragan <iliya5376@mail.com>
2016-10-20 00:28:55 +0400

    Add files via upload

--- bars.py
+++ bars.py
@@ -4,12 +4,29 @@ import math
 
 ## Загрузка файла, возвращает объект
 def load_json():
-	file = open("bars.json", "r")
-	result = file.read()
-	return json.loads(result)
+	try:
+	    with open("bars.json", "r") as json_file:
+	        json_content = json_file.read()
+	        return json.loads(json_content)
+	except FileNotFoundError:
+		return None
+
 
 ## Возвращает имя и число мест самого большого бара
 def get_max_bar(object):
+
+    bars_dict = {}
+    for item in object:
+    	# Получаем словарь типа имя бара:число мест
+        bars_dict[item["Cells"]["Name"]] = item["Cells"]["SeatsCount"]
+    # Сортируем словарь в порядке убывания, тем самым получая список
+    I = lambda x:x[1]
+    bars_list = sorted(bars_dict.items(), key=I, reverse = True)
+    # Забираем из списка первый элемент
+    return bars_list[0]
+    
+
+'''
 	max = object[0]["Cells"]["SeatsCount"]
 	name = ""
 	for item in object:
@@ -17,9 +34,22 @@ def get_max_bar(object):
 			max = item["Cells"]["SeatsCount"]
 			name = item["Cells"]["Name"]
 	return {"Name": name, "SeatsCount": max}
+'''
 
 ## Возвращает имя и число мест самого маленького бара
 def  get_min_bar(object):
+
+    bars_dict = {}
+    for item in object:
+    	# Получаем словарь типа имя бара:число мест
+        bars_dict[item["Cells"]["Name"]] = item["Cells"]["SeatsCount"]
+    # Сортируем словарь в полрядке возрастания, тем самым получая список
+    I = lambda x:x[1]
+    bars_list = sorted(bars_dict.items(), key=I, reverse = False)
+    # Забираем из списка первый элемент
+    return bars_list[0]
+
+'''
 	min = object[0]["Cells"]["SeatsCount"]
 	name = ""
 	for item in object:
@@ -27,6 +57,7 @@ def  get_min_bar(object):
 			min = item["Cells"]["SeatsCount"]
 			name = item["Cells"]["Name"]
 	return {"Name": name, "SeatsCount": min}
+'''
 
 ##Считает расстояние по дуге на Земле между двумя точками, заданными широтой и долготой в градусах
 ## Принимает в качестве параметров значения широт и долгот двух точек, расстояние между которыми требуется определить
@@ -47,7 +78,7 @@ def circle_distance(long1, lat1, long2, lat2):
 	distance = 6367444.6571225 * delta # умножаем угловое расстояние на радиус Земли
 	return distance
 
-## Возвращает геоданные (широту и долготу)
+## Возвращает словарь и геоданными(широту и долготу)
 def  get_geo_data(object):
 	return {"long" : object["Cells"]["geoData"]["coordinates"][1], "lat" : object["Cells"]["geoData"]["coordinates"][0]}
 
@@ -75,21 +106,28 @@ def get_nearest_bar(list, long, lat):
 	return {"Name" : name, "distance" : round(min_dist/1000, 2)}
 		
 
+def main():
+
+    list = load_json()
+    if list is not None:
+        max_bar = get_max_bar(list)
+        min_bar = get_min_bar(list)
+        print("Самый большой бар: ", max_bar[0], "Число мест: ", max_bar[1])
+        print("Самый маленький бар: ", min_bar[0], "Число мест: ", min_bar[1])
+
+        #e = circle_distance(55.688475, 37.909184, 41.988559, 21.463479)
+        #print(e)
+        print("Введите вашу широту")
+        long_data= input()
+        print("Введите вашу долготу")
+        lat_data = input()
+        nearest_bar = get_nearest_bar(list, long_data, lat_data)
+        print("Ближайший бар: ", nearest_bar["Name"], ", Близость: ", nearest_bar["distance"], " км")
 
 
-list = load_json()
 
-b = get_max_bar(list)
-c = get_min_bar(list)
-print("Самый большой бар: ", b["Name"], "Число мест: ", b["SeatsCount"])
-print("Самый маленький бар: ", c["Name"], "Число мест: ", c["SeatsCount"])
 
-#e = circle_distance(55.688475, 37.909184, 41.988559, 21.463479)
-#print(e)
-print("Введите вашу широту")
-long_data= input()
-print("Введите вашу долготу")
-lat_data = input()
-e = get_nearest_bar(list, long_data, lat_data)
-print("Ближайший бар: ", e["Name"], ", Близость: ", e["distance"], " км")
+
+if __name__ == '__main__':
+	main()
 
