a2e2dd267cc1a5ee5155573b37846815edf9e8a4
Dragan <iliya5376@mail.com>
2016-11-01 00:52:56 +0300

    Add files via upload

--- bars.py
+++ bars.py
@@ -1,115 +1,103 @@
 import json
 import math
 import sys
+import os.path
 
 
-## Загрузка файла, возвращает объект
-def load_json():
-    try:
-        with open(sys.argv[1], "r") as json_file:
+# Загрузка файла, возвращает объект
+def load_json(file_path):
+    if(os.path.exists(file_path)):
+        with open(file_path, "r") as json_file:
             json_content = json_file.read()
             return json.loads(json_content)
-    except FileNotFoundError:
+    else:
         return None
 
 
-## Возвращает имя и число мест самого большого бара
-def get_max_bar(object):
+# Возвращает словарь в виде имени и числа мест самого большого бара
+def get_max_bar(bar_list):
 
-    bars_seats_list = [] # Список числа мест для всех баров
-    for item in object:
-        bars_seats_list.append(item["Cells"]["SeatsCount"])
-
-    max_count = max(bars_seats_list) # Получаем максимальное число мест
-    for item in object:
-        if(item["Cells"]["SeatsCount"]==max_count):
-            return {"Name" : item["Cells"]["Name"], "SeatsCount" : item["Cells"]["SeatsCount"]}
+    max_count = max(bar["Cells"]["SeatsCount"] for bar in bar_list)  # Получаем максимальное число мест
+    for item in bar_list:
+        if(item["Cells"]["SeatsCount"] == max_count):
+            return {"Name": item["Cells"]["Name"],
+                    "SeatsCount": item["Cells"]["SeatsCount"]}
 
 
+# Возвращает словарь в виде имени и числа мест самого маленького бара
+def get_min_bar(bar_list):
 
-## Возвращает имя и число мест самого маленького бара
-def  get_min_bar(object):
+    min_count = min(bar["Cells"]["SeatsCount"] for bar in bar_list)  # Получаем максимальное число мест
+    for item in bar_list:
+        if(item["Cells"]["SeatsCount"] == min_count):
+            return {"Name": item["Cells"]["Name"],
+                    "SeatsCount": item["Cells"]["SeatsCount"]}
 
-    bars_seats_list = [] # Список числа мест для всех баров
-    for item in object:
-        bars_seats_list.append(item["Cells"]["SeatsCount"])
 
-    min_count = min(bars_seats_list) # Получаем максимальное число мест
-    for item in object:
-        if(item["Cells"]["SeatsCount"]==min_count):
-            return {"Name" : item["Cells"]["Name"], "SeatsCount" : item["Cells"]["SeatsCount"]}
-
-##Считает расстояние по дуге на Земле между двумя точками, заданными широтой и долготой в градусах
-## Принимает в качестве параметров значения широт и долгот двух точек, расстояние между которыми требуется определить
+# Считает расстояние между точками, заданными широтой и долготой в градусах
 def circle_distance(long1, lat1, long2, lat2):
     long1 = math.radians(long1)
     lat1 = math.radians(lat1)
     long2 = math.radians(long2)
     lat2 = math.radians(lat2)
-
-
     sin1 = math.sin(lat1)
     sin2 = math.sin(lat2)
     cos1 = math.cos(lat1)
     cos2 = math.cos(lat2)
     cos12 = math.cos(long2-long1)
-    delta = math.acos(sin1*sin2 + cos1*cos2*cos12) # Получаем угловое расстояние
+    delta = math.acos(sin1*sin2 + cos1*cos2*cos12)  # Получаем угловое расстояние
 
-    distance = 6367444.6571225 * delta # умножаем угловое расстояние на радиус Земли
+    distance = 6367444.6571225 * delta  # умножаем угловое расстояние на радиус Земли
     return distance
 
-## Возвращает словарь и геоданными(широту и долготу)
-def  get_geo_data(object):
-    return {"long" : object["Cells"]["geoData"]["coordinates"][1], "lat" : object["Cells"]["geoData"]["coordinates"][0]}
+
+# Возвращает словарь и геоданными(широту и долготу)
+def get_geo_data(bar_item):
+    return {"long": bar_item["Cells"]["geoData"]["coordinates"][1],
+            "lat": bar_item["Cells"]["geoData"]["coordinates"][0]}
 
 
-
-##Возвращает самый ближайший бар в виде имени и расстояния до него
-## Принимает в параметры список баров и текущие координаты
-def get_nearest_bar(list, long, lat):
+# Возвращает словрь в виде имени ближнего бара и расстояния до него
+def get_nearest_bar(bar_list, long, lat):
     long = float(long)
     lat = float(lat)
-    bar_name = list[0]["Cells"]["Name"]
-    geo_data = get_geo_data(list[0])
+    bar_name = bar_list[0]["Cells"]["Name"]
+    geo_data = get_geo_data(bar_list[0])
     long_data = geo_data["long"]
     lat_data = geo_data["lat"]
-    min_dist = circle_distance(long_data , lat_data, long, lat)
-    i = 1
-    for item in list:
+    min_dist = circle_distance(long_data, lat_data, long, lat)
+    for item in bar_list:
         geo_data = get_geo_data(item)
         long_data = geo_data["long"]
         lat_data = geo_data["lat"]
-        if (circle_distance(long_data, lat_data, long, lat)<min_dist):
+        if (circle_distance(long_data, lat_data, long, lat) < min_dist):
             min_dist = circle_distance(long_data, lat_data, long, lat)
             bar_name = item["Cells"]["Name"]
 
-    return {"Name" : bar_name, "distance" : round(min_dist/1000, 2)}
+    return {"Name": bar_name, "distance": round(min_dist/1000, 2)}
 
 
 def main():
 
-    list = load_json()
-    if list is not None:
-        max_bar = get_max_bar(list)
-        min_bar = get_min_bar(list)
-        print("Самый большой бар: ", max_bar["Name"], "Число мест: ", max_bar["SeatsCount"])
-        print("Самый маленький бар: ", min_bar["Name"], "Число мест: ", min_bar["SeatsCount"])
+    bar_list = load_json(sys.argv[1])
+    if bar_list is not None:
+        max_bar = get_max_bar(bar_list)
+        min_bar = get_min_bar(bar_list)
+        print("Самый большой бар: ", max_bar["Name"],
+              "Число мест: ", max_bar["SeatsCount"])
+        print("Самый маленький бар: ", min_bar["Name"],
+              "Число мест: ", min_bar["SeatsCount"])
 
         #e = circle_distance(55.688475, 37.909184, 41.988559, 21.463479)
         #print(e)
-        print("Введите вашу широту")
-        long_data= input()
-        print("Введите вашу долготу")
-        lat_data = input()
-        nearest_bar = get_nearest_bar(list, long_data, lat_data)
-        print("Ближайший бар: ", nearest_bar["Name"], ", Близость: ", nearest_bar["distance"], " км")
+        long_data = input("Введите вашу широту\n")
+        lat_data = input("Введите вашу долготу\n")
+        nearest_bar = get_nearest_bar(bar_list, long_data, lat_data)
+        print("Ближайший бар: ", nearest_bar["Name"],
+              ", Близость: ", nearest_bar["distance"], " км")
     else:
         print("Файл, путь до которого вы указали, не существует, или путь задан некорректно")
 
 
-
-
-
 if __name__ == '__main__':
     main()
-
